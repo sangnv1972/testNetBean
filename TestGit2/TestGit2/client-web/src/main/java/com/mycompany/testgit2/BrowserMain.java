846bdbd1cc0d61c14bc99bd6ae627a00aad7fa57
ADMINS <ADMINS@sangnv>
2023-04-11 16:47:40 +0700

    test

---
+++ TestGit2/TestGit2/client-web/src/main/java/com/mycompany/testgit2/BrowserMain.java
@@ -0,0 +1,10 @@
+package com.mycompany.testgit2;
+
+public class BrowserMain {
+    private BrowserMain() {
+    }
+
+    public static void main(String... args) throws Exception {
+        Main.onPageLoad();
+    }
+}
